dde7d1c4b755c2123508769b0db13df5110db9cf
Leo Viezens <leo.viezens@web.de>
2022-03-14 13:41:34 +0100

    Fix return type

--- src/Scorer/ScorerHolder.php
+++ src/Scorer/ScorerHolder.php
@@ -2,17 +2,15 @@
 
 namespace LeoVie\PhpCleanCode\Scorer;
 
-use Iterator;
-
 class ScorerHolder
 {
-    /** @param Iterator<int, Scorer> $scorers */
+    /** @param iterable<int, Scorer> $scorers */
     public function __construct(private iterable $scorers)
     {
     }
 
-    /** @return Iterator<int, Scorer> */
-    public function getScorers(): Iterator
+    /** @return iterable<int, Scorer> */
+    public function getScorers(): iterable
     {
         return $this->scorers;
     }
